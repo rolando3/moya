46e649959a92c19a355ab25e92291e07242c1d45
Rolando <rdeaguiar@yahoo.com>
2023-04-02 17:42:52 +0100

    model: Refine ProcessStep and Recipe

--- src/cs/Moya.Model/ProcessStep.cs
+++ src/cs/Moya.Model/ProcessStep.cs
@@ -2,4 +2,9 @@ namespace Moya.Model;
 
 public partial class ProcessStep
 {
+    public string Action { get; set; } = "";
+    public string[] Ingredients { get; set; } = new string[] {};
+    public string[] Results { get; set; } = new string[] {};
+    public string Description { get; set; } = "";
+    public TimeSpan Duration { get; set; } = TimeSpan.Zero;
 }
--- src/cs/Moya.Model/Recipe.cs
+++ src/cs/Moya.Model/Recipe.cs
@@ -4,6 +4,8 @@ namespace Moya.Model;
 
 public partial class Recipe
 {
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
     public Ingredients Ingredients { get; set; } = new Ingredients();
     public Process Process { get; set; } = new Process();
 }
